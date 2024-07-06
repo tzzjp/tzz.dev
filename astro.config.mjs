63a8502e2b8a65f5145f62ba9164da66c1e9d50f
tzzjp <tzz.zhao@gmail.com>
2024-07-06 13:41:53 +0900

    webAnalytics追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercelStatic({
+    webAnalytics: true,
+  }),
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja']
